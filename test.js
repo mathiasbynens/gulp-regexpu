f4ae7f19fdfffd0358192104420407c0ebf3aacd
Mathias Bynens <mathias@qiwi.be>
2014-08-25 09:27:58 +0200

    Add source map test
    Thanks to @danielhusar for providing a good example of how to do this: https://github.com/floridoo/gulp-sourcemaps/issues/25#issuecomment-53206051

--- test.js
+++ test.js
@@ -1,22 +1,15 @@
 var assert = require('assert');
 var gutil = require('gulp-util');
+var sourceMaps = require('gulp-sourcemaps');
 var regexpu = require('./index.js');
 
 it('should transpile ES6 regex to ES5 with regexpu', function(callback) {
 	var stream = regexpu();
 
 	stream.on('data', function(file) {
-		// TODO: this is never reached. Awaiting resolution of
-		// https://github.com/floridoo/gulp-sourcemaps/issues/25.
-		if (/\.map$/.test(file.path)) {
-			assert(/"version":3/.test(file.contents.toString()));
-			assert.equal(file.relative, 'fixture.js.map');
-			return;
-		}
-
 		assert(/\\uD834/.test(file.contents.toString()));
 		assert.equal(file.relative, 'fixture.js');
-	})
+	});
 
 	stream.on('end', callback);
 
@@ -29,4 +22,30 @@ it('should transpile ES6 regex to ES5 with regexpu', function(callback) {
 
 	stream.end();
 
+});
+
+it('should generate source maps', function(callback) {
+	var init = sourceMaps.init();
+	var write = sourceMaps.write();
+	init
+		.pipe(regexpu())
+		.pipe(write);
+
+	write.on('data', function(file) {
+		assert.equal(file.sourceMap.mappings, 'AAAA,CAAC,CAAC,EAAE,EAAE,6BAA0B');
+		var contents = file.contents.toString();
+		assert(/\\uD834/.test(contents));
+		assert(/sourceMappingURL=data:application\/json;base64/.test(contents));
+		callback();
+	});
+
+	init.write(new gutil.File({
+		'cwd': __dirname,
+		'base': __dirname + '/fixture',
+		'path': __dirname + '/fixture/fixture.js',
+		'contents': new Buffer('var x = /[\\u{1D306}-\\u{1D308}]/u;'),
+		'sourceMap': ''
+	}));
+
+	init.end();
 });
